333d6e0a75c14a032e559ff7fdebceaf0a0f3b94
seifemad <eseif102@gmail.com>
2026-09-14 15:07:04 +0300

    Release v3.0.70

--- functions.php
+++ functions.php
@@ -378,7 +378,7 @@ add_action( 'wp_enqueue_scripts', 'ecm_enqueue_assets' );
 
 
 // ── THEME VERSION CONSTANT ───────────────────────────────────
-define( 'ECM_VERSION', '3.0.69' );
+define( 'ECM_VERSION', '3.0.70' );
 
 
 // ── INCLUDE: FRONT PAGE CUSTOMIZER ──────────────────────────
